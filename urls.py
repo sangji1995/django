e3d3d9eb249e011a3fddaf325563236193afe0da
sangji1995 <69757364+sangji1995@users.noreply.github.com>
2020-10-28 22:44:54 +0900

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,27 +1,14 @@
-"""config URL Configuration
+from django.urls import path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-# ---------------------------------------- [edit] ---------------------------------------- #
-from django.urls import path, include
-# ---------------------------------------------------------------------------------------- #
+from . import views
 
+app_name='pybo'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # ---------------------------------------- [edit] ---------------------------------------- #
-    path('pybo/', include('pybo.urls')),
-    # ---------------------------------------------------------------------------------------- #
-]
 
+    path('', views.index, name='index'),
+
+    # ---------------------------------------- [edit] ---------------------------------------- #
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ---------------------------------------------------------------------------------------- #
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+]
